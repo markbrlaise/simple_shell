fea83b5279db08c1548d133fce2da46da73c86f8
markbrlaise <markkiguli10@gmail.com>
2022-11-10 06:46:10 -0800

    Hola

---
+++ hi_betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - making betty happy with the main function
+ *
+ *Return: (0) success
+ *
+ */
+
+int main(void)
+{
+	printf("Hi Betty... Hope this makes you happy!\n");
+
+	return (0);
+}
